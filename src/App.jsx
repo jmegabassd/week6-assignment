19c929e8cffa87284f5d3c2b17471476bf27a552
jmegabassd <jmegabassd@gmail.com>
2025-09-18 12:59:43 +0100

    Task: add Thumbails component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Thumbnails from "./components/Thumbnails";
+import MainImage from "./components/MainImage";
 
 export default function App() {
   return (
     <>
       <Thumbnails></Thumbnails>
+      <MainImage></MainImage>
     </>
   );
 }
---
+++ src/components/MainImage.jsx
@@ -0,0 +1,9 @@
+import "./MainImage.css";
+
+export default function MainImage() {
+  return (
+    <>
+      <div className="main-image-cont">Main Image Container</div>
+    </>
+  );
+}
